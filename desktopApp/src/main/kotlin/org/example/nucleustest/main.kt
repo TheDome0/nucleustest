c571e2beb45bc02b816bac31e73ecc1b226bcafe
dominik.stoeby <dominik.stoeby@gmail.com>
2026-07-19 16:38:29 +0200

    update versions

--- desktopApp/src/main/kotlin/org/example/nucleustest/main.kt
+++ desktopApp/src/main/kotlin/org/example/nucleustest/main.kt
@@ -4,7 +4,7 @@ import dev.nucleusframework.application.DecoratedWindow
 import dev.nucleusframework.application.NucleusBackend
 import dev.nucleusframework.application.nucleusApplication
 
-fun main() = nucleusApplication(backend = NucleusBackend.Tao) {
+fun main() = nucleusApplication(backend = NucleusBackend.Awt) {
     DecoratedWindow(
         onCloseRequest = ::exitApplication,
         title = "nucleustest",
